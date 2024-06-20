b53adef0ee18a0fd6dcf0bea093d4e63bb7085aa
e-vinyl <nuno.v.barreto@gmail.com>
2024-06-20 23:29:40 +0200

    Makes it so the cannon can be armed with a ball texture

--- Assets/Scripts/CannonComponent.cs
+++ Assets/Scripts/CannonComponent.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 [RequireComponent(typeof(SpriteRenderer))]
 [RequireComponent(typeof(AimingComponent))]
 public class CannonComponent : MonoBehaviour
 {
+
     public int CurrentTilt
     {
         set
@@ -14,12 +16,27 @@ public class CannonComponent : MonoBehaviour
         }
     }
 
+    public bool CanFire
+    {
+        get
+        {
+            return canFire;
+        }
+    }
+
     [SerializeField]
     private CannonBallComponent cannonPrefab;
+
+    [SerializeField]
+    private Sprite defaultImage;
 
     private AimingComponent aimingComponent;
 
     private int currentTilt;
+
+    private bool canFire = false;
+
+    private Sprite ballTexture;
 
     // Start is called before the first frame update
     void Start()
@@ -27,9 +44,31 @@ public class CannonComponent : MonoBehaviour
         aimingComponent = GetComponent<AimingComponent>();
     }
 
-    public void FireCannon(Texture2D texture)
+    public void StartLoading()
     {
-        GameObject cannonBall = Instantiate(cannonPrefab.gameObject, new Vector3(0, 0, 0), Quaternion.identity);
-        aimingComponent.AttachObject(new AimingComponent.AttachableObject(cannonBall, currentTilt));
+        canFire = true;
+    }
+
+    public void ChargeCannon(Sprite texture)
+    {
+        ballTexture = texture;
+    }
+
+    public void FireCannon()
+    {
+        if(!canFire)
+        {
+            return;
+        }
+
+        CannonBallComponent cannonBall = Instantiate<CannonBallComponent>(cannonPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        aimingComponent.AttachObject(new AimingComponent.AttachableObject(cannonBall.gameObject, currentTilt));
+        cannonBall.gameObject.transform.localScale *= ballTexture ? 1f : 0.5f;
+        cannonBall.BallRenderer.sprite = ballTexture ? ballTexture : defaultImage;
+
+        if (ballTexture)
+        {
+            ballTexture = null;
+        }
     }
 }
